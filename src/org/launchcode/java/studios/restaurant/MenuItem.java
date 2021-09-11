74aa0979c224f32ba3cf5ebb7f781939f7b6a214
Jenn <hashbrowns120@gmail.com>
2021-09-11 14:16:49 -0500

    started restaurant studio

---
+++ src/org/launchcode/java/studios/restaurant/MenuItem.java
@@ -0,0 +1,12 @@
+package org.launchcode.java.studios.restaurant;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class MenuItem {
+    private String foodName;
+    private String foodDescription;
+    private float price;
+    private Date dateAdded;
+    private String foodType;
+    private boolean isNew;
+}
