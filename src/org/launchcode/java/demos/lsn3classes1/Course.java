ecb25fd16084405c8ad9a4a34484e5c5c2cd7a10
Jenn <hashbrowns120@gmail.com>
2021-09-11 13:40:39 -0500

    exercises for chapter 4 finished

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,10 @@
+package org.launchcode.java.demos.lsn3classes1;
+import java.util.ArrayList;
+
+public class Course {
+    private String courseName;
+    private Teacher instructor;
+    private ArrayList<String> enrolledStudents;
+
+
+}
